6a84b62819d4eebe37f69432a237300ecdd615e5
LucasWerey <lucas.werey@hotmail.com>
2023-12-21 15:12:52 +0100

    fix logo

--- src/components/Indicators/Loader/Loader.spec.ts
+++ src/components/Indicators/Loader/Loader.spec.ts
@@ -7,4 +7,31 @@ describe('Loader.vue', () => {
     const wrapper = mount(Loader)
     expect(wrapper.html()).toContain('class="loader"')
   })
+
+  it('computes primary correctly', () => {
+    const wrapper = mount(Loader, {
+      props: {
+        color: 'primary'
+      }
+    })
+    expect(wrapper.vm.color).toBe('primary')
+  })
+
+  it('computes secondary correctly', () => {
+    const wrapper = mount(Loader, {
+      props: {
+        color: 'secondary'
+      }
+    })
+    expect(wrapper.vm.color).toBe('secondary')
+  })
+
+  it('computes loaderSize correctly', () => {
+    const wrapper = mount(Loader, {
+      props: {
+        size: 'small'
+      }
+    })
+    expect(wrapper.vm.size).toBe('small')
+  })
 })
